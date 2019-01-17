d4d3148f307967a37477dd649600e0035bc5ac5f
Andrey Platonov <poluandrey@gmail.com>
2019-01-17 19:04:21 +0700

    remove package before build

--- build-archlinux.sh
+++ build-archlinux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pkgname="zabbix-agent-extension-rabbitmq"
-rm -rf src/ pkg/ $pkgname
+rm -rf src/ pkg/ $pkgname *.tar.xz
 makepkg -Cod; PKGVER=$(cd $(pwd)/src/$pkgname/ && make ver) makepkg -esd
